859998665288bb8ed4384a9e988e74898eda24d2
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-07-24 17:59:42 -0400

    Add code to solve Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let sALength = stringArray.length;
+    let sAReversed = [];
+    for (let i = sALength - 1; i >= 0; i--) {
+        sAReversed.push(stringArray[i]);
+    }
+    return sAReversed.join("");
 };
 
 // Do not edit below this line
