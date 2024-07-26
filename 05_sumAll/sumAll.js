9cd1d275e3ea28cb7b3f47591d133600d3a60fd3
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-07-26 13:15:28 -0400

    Add code to solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = Number();
+    if (first < 0 || second < 0 || Number.isInteger(first) === false || Number.isInteger(second) === false) {
+        return 'ERROR';
+    } else if (second > first) {
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (first > second) {
+        for (let i = second; i <= first; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
