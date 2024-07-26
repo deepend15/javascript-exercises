4be279ef07f1946eec610fe23fa0d4c70feb8466
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-07-26 13:32:52 -0400

    Add code to solve leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year / 4) === true 
+    && (Number.isInteger(year / 100) === false 
+    || Number.isInteger(year / 400) === true)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
