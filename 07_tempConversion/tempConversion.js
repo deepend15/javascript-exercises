f97448bc7b44aa569477343d1855ba901e8d26a3
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-07-26 13:48:40 -0400

    Add code to solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+  let celsiusTempRounded = Math.round(celsiusTemp * 10) / 10;
+  return celsiusTempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * (9 / 5) + 32);
+  let fahrenheitTempRounded = Math.round(fahrenheitTemp * 10) / 10;
+  return fahrenheitTempRounded;
 };
 
 // Do not edit below this line
