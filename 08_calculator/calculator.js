d2d37c73177795369da89945753b29ba3f97a8e4
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-08-20 14:18:52 -0400

    Add solution for calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {
+    return total + current;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, current) => {
+    return total * current;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    let total = num;
+    for (let i = num - 1; i > 0; i--) {
+      total *= i;
+    };
+    return total;
+  }
 };
 
 // Do not edit below this line
