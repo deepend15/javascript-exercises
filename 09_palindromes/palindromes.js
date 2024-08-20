464a462ca3c2a532c2d6f80209373bcf40adc9a8
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-08-20 16:05:29 -0400

    Add code to solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = string.toLowerCase().split("");
+    let arrLetters = arr.filter((letter) => letter !== ' ' && letter !== ',' && letter !== '.' && letter !== '!');
+    let arrLettersReversed = arrLetters.slice().reverse();
+    let strLetters = arrLetters.join('');
+    let strLettersReversed = arrLettersReversed.join('');
+    if (strLetters === strLettersReversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
