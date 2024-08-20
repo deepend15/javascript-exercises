63b624ca6b18d5cf9260647d4567a262725a0007
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-08-20 18:12:34 -0400

    Add code to solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number(num) === 0) {
+        return 0;
+    } else if (Number(num) < 0) {
+        return 'OOPS';
+    } else {
+        function createFibonacciArray(num) {
+            let arr = [];
+            for (let i = 0; i < num; i++) {
+                if (i === 0 || i === 1) {
+                    arr[i] = 1;
+                } else {
+                arr[i] = arr[i - 2] + arr[i - 1];
+                }
+            };
+            return arr;
+        }
+        let fibArr = createFibonacciArray(Number(num));
+        return fibArr[Number(num) - 1];
+    }
 };
 
 // Do not edit below this line
