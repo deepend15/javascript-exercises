fedd010981222a984a4388ba9c4d12fe25387acb
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-08-21 13:06:45 -0400

    Add code to solve findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let peopleCopy = people.slice();
+    for (const person of peopleCopy) {
+        if (person.yearOfDeath === undefined) {
+            let thisYear = new Date().getFullYear();
+            person.age = thisYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+    peopleCopy.sort((a, b) => {
+        return b.age - a.age;
+    });
+    let oldestPerson = peopleCopy[0];
+    return oldestPerson;
 };
 
 // Do not edit below this line
